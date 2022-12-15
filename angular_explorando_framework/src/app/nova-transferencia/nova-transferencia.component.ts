7135d430a8d6db1a4c37e506e922a5223b67f3ad
adenhercules <adenhercules07@gmail.com>
2022-12-14 21:54:53 -0400

    06-criando o primeiro component

---
+++ angular_explorando_framework/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "nova-transferencia",
+    templateUrl: "./transferencia.component.html",
+    styleUrls: ["./transferencia.component.scss"]
+})
+
+export class TransferenciaComponent {
+
+    
+}
